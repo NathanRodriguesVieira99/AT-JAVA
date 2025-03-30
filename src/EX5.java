79083b456dc1a50e8425fa57c2815de7f1dbf0c2
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-30 13:51:37 -0300

    feat: ex 5 done

---
+++ src/EX5.java
@@ -0,0 +1,17 @@
+public class EX5 {
+public static void main(String[] args) {
+    System.out.println("Content-Type: text/html");
+    System.out.println();
+
+    System.out.println("<!DOCTYPE html>");
+        System.out.println("<html>");
+        System.out.println("<head>");
+        System.out.println("<title>Saudação</title>");
+        System.out.println("<meta charset=\"UTF-8\">");
+        System.out.println("</head>");
+        System.out.println("<body>");
+        System.out.println("<h1>Olá, Terráqueos!</h1>");
+        System.out.println("</body>");
+        System.out.println("</html>");
+}
+}
